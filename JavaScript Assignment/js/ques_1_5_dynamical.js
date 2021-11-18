dea0fd736161c43ec87687338dbab02933d9f5f9
Jyoti Chaudhary <55753178+jyoti-byte@users.noreply.github.com>
2021-11-18 09:51:48 +0545

    Add files via upload

---
+++ JavaScript Assignment/js/ques_1_5_dynamical.js
@@ -0,0 +1,7 @@
+//dynamically typed languages
+
+//In simple words, dynamically typed language means the type of variables 
+//is checked during the run-time, even the code is dynamic.
+//JS is called a dynamically typed language because the type of variable
+// is not defined during the declaration. Also same variable can be used 
+//to store the different data types as number, string, object, Boolean and so on.
